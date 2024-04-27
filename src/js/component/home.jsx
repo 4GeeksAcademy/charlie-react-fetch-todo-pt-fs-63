1e786e58e7a2f49a5ba7ca860c87dfce1c827704
Charlie Willoughby <104572309+CWilloughby98@users.noreply.github.com>
2024-04-27 11:36:24 +0000

    first

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,11 +1,55 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Home = () => {
 
 	const [list, setList] = useState(["Delete this!"])
 	const [inputValue, setInputValue] = useState("")
+	const [user, setUser] = useState("charlie")
+	const [userInput, setUserInput] = useState("")
+////////////////////////////////////////////////////////////
+	const createUser = async (username) => {
+		await fetch(`https://playground.4geeks.com/todo/users/${username}`, {
+			method: "POST"
+		}).then(res => {
+			if(!res.ok) {
+				alert(`User already exists`)
+			} else {
+				console.log(`Creating user ${username}`)
+			}
+		})
+	}
 
+	const getUser = async (username) => {
+		await fetch(`https://playground.4geeks.com/todo/users/${username}`)
+        .then(res => {
+            if (!res.ok) {
+                createUser(username);
+            } else {
+                return res.json();
+            }
+        }).then(u => {
+            if (u) {
+                setUser(u);
+                console.log(u);
+            }
+        });
+	}
+
+	const validateUser = (e) => {
+		if(e.key === "Enter") {
+			setUser(userInput);
+			userInput === "" ? console.log("Abort, input cannot be empty") : setUserInput("")
+			createUser(user)
+		}
+	}
+	
+	useEffect(() => {
+		//getUser(user)
+	}, [])
+	
+
+////////////////////////////////////////////////////////////
 	const handleAddItem = () => {
 		setList([...list, inputValue])
 		setInputValue("")
@@ -19,19 +63,25 @@ const Home = () => {
 		}
 	}
 	const handleDelete = (id) => {
-		setList(list.filter((_elem, index) => index !== id))
+		const newList = list.filter((_elem, index) => index !== id)
+		setList([...newList])
 	}
-
 	const checkItemNumber = () => {
 		if (list.length === 0) {
 			return "No tasks, add a task"
 		} else return `${list.length} ${list.length === 1 ? "item" : "items"} left`
 	}
-
+////////////////////////////////////////////////////////////
 	return (
 		<div className="text-center pt-5">
 			<h1 className="text-secondary font-monospace mb-0">ToDo List</h1>
 			<small className="text-secondary font-monospace">By Charlie</small>
+			<div className="mx-auto col-7">
+				<div className="input-group">
+					<span className="input-group-text">Add a User</span>
+					<input onChange={e => setUserInput(e.target.value)} onKeyDown={validateUser} value={userInput} className="form-control" type="text" />
+				</div>
+			</div>
 			<div className="col-7 col-xl-5 mx-auto mt-3 d-flex flex-column">
 				<input id="input" type="text" className="form-control border rounded-0 py-3 px-4 fs-5" placeholder="What needs to be done?" 
 				onChange={e => 
